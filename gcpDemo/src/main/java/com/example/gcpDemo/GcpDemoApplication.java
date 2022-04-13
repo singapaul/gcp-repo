316538a0931857b51580809377ddd8d6c0d1b36f
Paul <paulmichaelhardman@gmail.com>
2022-04-13 11:50:00 +0100

    first commit

---
+++ gcpDemo/src/main/java/com/example/gcpDemo/GcpDemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.gcpDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDemoApplication.class, args);
+	}
+
+
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Hello, I'm on the cloud";
+	}
+
+
+
+}
